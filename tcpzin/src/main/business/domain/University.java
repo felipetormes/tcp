2e7cd6ef0b6c1b3264ed1d43a34ad47efd40f7e9
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2016-05-30 18:13:36 -0300

    Completa classe university

--- tcpzin/src/main/business/domain/University.java
+++ tcpzin/src/main/business/domain/University.java
@@ -1,5 +1,16 @@
 package main.business.domain;
 
 public class University {
-
+	private String name;
+	
+	public University(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
 }
+
+
+
