becd37879407876a669da36515890b85f0c6796e
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2016-05-30 20:41:19 -0300

    Implementa corpo da classe Paper

--- tcpzin/src/main/business/domain/Paper.java
+++ tcpzin/src/main/business/domain/Paper.java
@@ -1,5 +1,65 @@
 package main.business.domain;
 
-public class Paper {
+import java.util.List;
 
+public class Paper {
+	private int id;
+	private String title;
+	private Researcher author;
+	private Conference conference;
+	private Topic researchTopic;
+	private List<Review> reviews;
+	private int last_id;
+	private double averageGrade;
+	private double allGrade;
+	
+	public Paper (String title, Researcher author, int id, Topic researchTopic) {
+		this.title = title;
+		this.author = author;
+		this.id = id;
+		this.researchTopic = researchTopic;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public Researcher getAuthor() {
+		return author;
+	}
+	
+	public void setConference(Conference conference) {
+		this.conference = conference;
+	}
+	
+	public Conference getConference() {
+		return conference;
+	}
+	
+	public Topic getResearchTopic () {
+		return researchTopic;
+	}
+	
+	public void addReview(Review review) {
+		reviews.add(review);            // Add review in reviews list
+
+	}
+	
+	public List<Review> getReviews() {
+		return reviews;
+	}
+	
+	public double getAverageGrade() {
+        /*
+		for(Review review : reviews){
+        	allGrade += review.getGrade();
+        }
+        averageGrade = allGrade / reviews.size();
+		*/
+		return -1;
+	}
 }
