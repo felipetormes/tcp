ed5614db540a579b0ef05ac862d792599ec460df
Eduardo Brito <Eduardo Brito>
2016-05-30 20:39:54 -0300

    Modificacoes nas classes da UI, adicionado metodos que faltavam

--- tcpzin/src/main/ui/text/ConferenceTextInterface.java
+++ tcpzin/src/main/ui/text/ConferenceTextInterface.java
@@ -1,5 +1,17 @@
 package main.ui.text;
 
-public class ConferenceTextInterface {
+import main.ui.ConferenceUI;
+
+public class ConferenceTextInterface extends ConferenceUI {
+
+	public static String EXIT_CODE = "E" ;
+
+	public String showMenu() {
+		return null;
+	}
+
+	public void showUI() {
+
+	}
 
 }
