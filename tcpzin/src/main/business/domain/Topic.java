0e71823bfcb6642ad8538bb25cd2440a9e8f3276
Luís Möllmann <luisgustavo181@gmail.com>
2016-05-30 18:10:38 -0300

    completa classe

--- tcpzin/src/main/business/domain/Topic.java
+++ tcpzin/src/main/business/domain/Topic.java
@@ -1,5 +1,13 @@
 package main.business.domain;
 
 public class Topic {
-
+	private String name;
+	
+	public Topic(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
