34dfa9e28cbf9b9d125503cde589706b0b62f568
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2016-05-30 18:49:18 -0300

    Adiciona throw exception na classe university

--- tcpzin/src/main/business/domain/University.java
+++ tcpzin/src/main/business/domain/University.java
@@ -1,10 +1,16 @@
 package main.business.domain;
 
+import main.exceptions.InvalidNameException;
+
 public class University {
 	private String name;
 	
-	public University(String name){
-		this.name = name;
+	public University(String name) throws InvalidNameException {
+		if (name != null) {
+			this.name = name;
+		} else {
+			throw new InvalidNameException("Invalid university name.");
+		}
 	}
 	
 	public String getName(){
