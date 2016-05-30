a3427368ac9f875c4fb195feb5c963938030f622
Luís Möllmann <luisgustavo181@gmail.com>
2016-05-30 18:24:30 -0300

    adiciona pacote com excessões e a exceção InvalidNameException

---
+++ tcpzin/src/main/exceptions/InvalidNameException.java
@@ -0,0 +1,13 @@
+package main.exceptions;
+
+public class InvalidNameException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidNameException() {
+	}
+	
+	public InvalidNameException(String message) {
+		super(message);
+	}
+}
