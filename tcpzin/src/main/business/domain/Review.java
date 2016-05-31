f39fd59e58d8a7440f667a99bcdcc5e7f779588b
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2016-05-30 21:28:12 -0300

    Completa classe Review

--- tcpzin/src/main/business/domain/Review.java
+++ tcpzin/src/main/business/domain/Review.java
@@ -1,5 +1,48 @@
 package main.business.domain;
 
 public class Review {
-
+	private Paper paper;
+	private boolean isPending;
+	private Researcher reviewer;
+	private double grade;
+	
+	public Review(Paper paper, Researcher reviewer, double grade){
+		this.paper = paper;
+		this.reviewer = reviewer;
+		this.setGrade(grade);
+		this.isPending = false;
+	}
+	
+	public Review(Paper paper, Researcher reviewer) {
+		this(paper, reviewer, -1);
+		this.isPending = true;
+	}
+	
+	public void setPaper (Paper paper) {
+		this.paper = paper;
+	}
+	
+	public Paper getPaper() {
+		return paper;
+	}
+	
+	public void setReviewer (Researcher reviewer) {
+		this.reviewer = reviewer;
+	}
+	
+	public Researcher getReviewer() {
+		return reviewer;
+	}
+	
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	
+	public double getGrade() {
+		return grade;
+	}
+	
+	public boolean isPendingGrade() {
+		return isPending;
+	}
 }
