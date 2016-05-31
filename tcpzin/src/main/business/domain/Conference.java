0214a11668cea9c68e93ae16ddddaeaa4225aa52
Luís Möllmann <luisgustavo181@gmail.com>
2016-05-30 21:07:30 -0300

    adiciona o básico em Conference

--- tcpzin/src/main/business/domain/Conference.java
+++ tcpzin/src/main/business/domain/Conference.java
@@ -1,5 +1,48 @@
 package main.business.domain;
 
-public class Conference {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Conference {
+	private String initials;
+	private List<Paper> papers;
+	private List<Researcher> committeeMembers;
+	
+	public Conference(String initials) {
+		this.initials = initials;
+		this.papers = new ArrayList<Paper>();
+		this.committeeMembers = new ArrayList<Researcher>();
+	}
+	
+	public boolean hasEmptyGrade() {
+		/* if one paper has less reviews than this conference has committee
+		 * members, then some grade is empty.
+		 */
+		for (Paper paper : papers) {
+			if (paper.getReviews().size() < committeeMembers.size()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void addCommitteeMember(Researcher member) {
+		committeeMembers.add(member);
+	}
+	
+	public void addPaper(Paper paper) {
+		papers.add(paper);
+	}
+	
+	public String getInitials() {
+		return initials;
+	}
+	
+	public List<Paper> getPapers() {
+		return papers;
+	}
+	
+	public List<Researcher> getCommitteeMembers() {
+		return committeeMembers;
+	}
 }
