8048ebd1a18daa1010b6aacbb9a81da518a4a647
Eduardo Brito <Eduardo Brito>
2016-05-31 20:19:46 -0300

    Colocado 'esqueleto' inicial da classe PaperManagementServiceImpl para poder iniciar a pensar na implementacao das classes command

--- tcpzin/src/main/business/impl/PapersManagementServiceImpl.java
+++ tcpzin/src/main/business/impl/PapersManagementServiceImpl.java
@@ -1,7 +1,68 @@
 package main.business.impl;
 
-import main.business.PapersManagementService;
+import java.util.ArrayList;
 
-public class PapersManagementServiceImpl implements PapersManagementService {
+import main.business.PapersManagementService;
+import main.business.domain.Conference;
+import main.business.domain.Paper;
+import main.business.domain.Researcher;
+import main.data.Database;
+
+public class PapersManagementServiceImpl  implements PapersManagementService {
+
+	private Database database;
+
+
+	public PapersManagementServiceImpl(Database database) {
+
+	}
+
+	private ArrayList<Researcher> getCommitteeMembersCandidates(Conference conference) {
+		return null;
+	}
+
+	private ArrayList<Researcher> getReviewersFromCandidates(ArrayList<Researcher> candidates) {
+		return null;
+	}
+
+
+	/**
+	 * @see PapersManagementService#allocPapersToReviewers(Conference, int)
+	 */
+	public void allocPapersToReviewers(Conference conference, int numReviewers) {
+
+	}
+
+
+	/**
+	 * @see PapersManagementService#setGradeToPaper(Paper, Reviewer, Double)
+	 */
+	public void setGradeToPaper(Paper paper, Researcher reviewer, Double grade) {
+
+	}
+
+
+	/**
+	 * @see PapersManagementService#selectPapersByAverage(Conference)
+	 */
+	public Map<Paper,Boolean> selectPapersByAverage(Conference conference) {
+		return null;
+	}
+
+
+	/**
+	 * @see PapersManagementService#GetAllConferences()
+	 */
+	public ArrayList<Conference> GetAllConferences() {
+		return null;
+	}
+
+
+	/**
+	 * @see PapersManagementService#GetAllPapers()
+	 */
+	public ArrayList<Paper> GetAllPapers() {
+		return null;
+	}
 
 }
