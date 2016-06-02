f0788a8ef714262fd2c26499a20d98e9c0bfa295
Eduardo <eduardobrito14@gmail.com>
2016-06-02 10:37:46 -0300

    Adiciona mais funcionalidades (leitura de valores de entrada) no UIUtils!

--- tcpzin/src/main/ui/text/UIUtils.java
+++ tcpzin/src/main/ui/text/UIUtils.java
@@ -1,6 +1,8 @@
 package main.ui.text;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
@@ -9,11 +11,16 @@ public class UIUtils {
 
 	public static final UIUtils INSTANCE = new UIUtils();
 	public static final String PROPERTY_RESOURCE_BUNDLE = "main.resources.globalMessages";
-	
-	
+	private final BufferedReader reader;
 	private static ResourceBundle bundle;
+	
+	
+	public UIUtils() {
+		UIUtils.bundle = ResourceBundle.getBundle(PROPERTY_RESOURCE_BUNDLE);
+		this.reader = new BufferedReader(new InputStreamReader(System.in));
+	}
 	public static String getText(String key) {
-		bundle = ResourceBundle.getBundle(PROPERTY_RESOURCE_BUNDLE);
+		
 		String text = null;
 		if (text == null) {
 			try {
@@ -25,6 +32,55 @@ public class UIUtils {
 		return text;
 	}
 	
+	public Double readDouble(String field) {
+		Double value = null;
+		while (value == null) {
+			try {
+				System.out.print(UIUtils.getText(field) + ": ");
+				value = new Double(reader.readLine());
+			} catch (NumberFormatException nfe) {
+				System.out.println(UIUtils
+						.getText("exception.formato.double"));
+				
+			} catch (Exception e) {
+				
+			}
+		}
+		return value;
+	}
+
+	public Integer readInteger(String field) {
+		Integer value = null;
+		while (value == null) {
+			try {
+				if (field != null)
+					System.out.print(UIUtils.getText(field) + ": ");
+				value = new Integer(reader.readLine());
+			} catch (NumberFormatException nfe) {
+				System.out.println(UIUtils
+						.getText("exception.formato.int"));
+	
+			} catch (Exception e) {
+	
+			}
+		}
+		return value;
+	}
+
+	
+	public String readString(String field) {
+		String value = null;
+		while (value == null) {
+			try {
+				if (field != null)
+					System.out.print(UIUtils.getText(field) + ": ");
+				value = reader.readLine();
+			} catch (Exception e) {
+	
+			}
+		}
+		return value;
+	}
 	
 
 	
