58f939163905a405f74298a4681f748d8e952343
Luís Möllmann <luisgustavo181@gmail.com>
2016-06-02 16:19:09 -0300

    testa initResearchers a Database

--- tcpzin/src/test/data/DatabaseTest.java
+++ tcpzin/src/test/data/DatabaseTest.java
@@ -1,30 +1,32 @@
 package test.data;
 
-import static org.junit.Assert.*;
-
 import java.util.List;
 
+import main.business.domain.Researcher;
+import main.data.Database;
 
 import org.junit.Before;
-import org.junit.Test;
-
-import main.data.Database;
 
 
 
 public class DatabaseTest {
-		Database database;
+	Database database;
+
+	public static void main(String[] args) {
+		Database database = new Database();
+		List<Researcher> researchers = database.getResearchers();
 		
-		public static void main(String[] args) {
-			Database database = new Database();
-		}
-		
-		/**
-		 * @throws java.lang.Exception
-		 */
-		@Before
-		public void setUp() throws Exception {
-			database = new Database();
+		for (Researcher researcher : researchers) {
+			System.out.println(researcher);
 		}
 	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		database = new Database();
+	}
+}
 
