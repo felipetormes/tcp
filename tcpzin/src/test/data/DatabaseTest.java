18ae30127d510a605f447bbd9e50b626ed3007e5
Luís Möllmann <luisgustavo181@gmail.com>
2016-06-02 16:59:32 -0300

    remove dependencias de List, Researcher e Conference usando toString de Database

--- tcpzin/src/test/data/DatabaseTest.java
+++ tcpzin/src/test/data/DatabaseTest.java
@@ -1,9 +1,5 @@
 package test.data;
 
-import java.util.List;
-
-import main.business.domain.Conference;
-import main.business.domain.Researcher;
 import main.data.Database;
 
 import org.junit.Before;
@@ -15,16 +11,7 @@ public class DatabaseTest {
 
 	public static void main(String[] args) {
 		Database database = new Database();
-		List<Researcher> researchers = database.getResearchers();
-		List<Conference> conferences = database.getConferences();
-		
-		for (Researcher researcher : researchers) {
-			System.out.println(researcher);
-		}
-		
-		for (Conference conference : conferences) {
-			System.out.println(conference);
-		}
+		System.out.println(database);
 	}
 	
 	/**
