941021cf4f96fccef5dd3a83b7b6c94b2b1d18ab
Eduardo <eduardobrito14@gmail.com>
2016-06-03 14:31:13 -0300

    Cria BusinessDomainException para erros relacionados com classes do dominio. Alem disso arruma PaperAttributuion com esses exceptions

--- tcpzin/src/main/ui/text/ConferenceTextInterface.java
+++ tcpzin/src/main/ui/text/ConferenceTextInterface.java
@@ -1,6 +1,5 @@
 package main.ui.text;
 
-
 import main.business.PapersManagementService;
 import main.exceptions.InvalidInputException;
 import main.ui.ConferenceUI;
@@ -11,50 +10,65 @@ import main.ui.text.command.PapersSelectionCommand;
 
 public class ConferenceTextInterface extends ConferenceUI {
 
-	public static String EXIT_CODE = "E" ;
+	public static String EXIT_CODE = "E";
 	private String option;
 	private ConferenceUICommand conferenceUIcommand;
 	private PapersManagementService papersManagementService;
-	public ConferenceTextInterface(PapersManagementService papersManagementService){
+
+	public ConferenceTextInterface(
+			PapersManagementService papersManagementService) {
 		this.papersManagementService = papersManagementService;
 	}
+
 	public String showMenu() {
 		StringBuffer sb = new StringBuffer();
-		sb.append(UIUtils.getText("message.options"))
-				.append(":\n");
+		sb.append(UIUtils.getText("message.options")).append(":\n");
 		sb.append(UIUtils.getText("message.options.alocacao")).append("\n");
 		sb.append(UIUtils.getText("message.options.atribuicao")).append("\n");
 		sb.append(UIUtils.getText("message.options.selecao")).append("\n");
-	
 
 		return sb.toString();
-		
+
 	}
 
+	@Override
+	public void addCommand(String key, ConferenceUICommand command) {
+		super.addCommand(key, command);
+	}
+
+	@Override
 	public void showUI() {
-		System.out.println(showMenu());
-		option = UIUtils.readString(UIUtils.getText("message.choose.option"));
-		try {
-			if(option.contentEquals("A")){
-				conferenceUIcommand = new PapersAllocationCommand(papersManagementService);
-				conferenceUIcommand.execute();
-			}else{
-				if(option.contentEquals("T")){
-					conferenceUIcommand = new PapersGradesAttributionCommand(papersManagementService);
+		do {
+			System.out.println(showMenu());
+			option = UIUtils.readString(UIUtils
+					.getText("message.choose.option"));
+			try {
+				if (option.contentEquals("A")) {
+					conferenceUIcommand = new PapersAllocationCommand(
+							papersManagementService);
 					conferenceUIcommand.execute();
-				}else{
-					if(option.contentEquals("S")){
-						conferenceUIcommand = new PapersSelectionCommand(papersManagementService);
+				} else {
+					if (option.contentEquals("T")) {
+						conferenceUIcommand = new PapersGradesAttributionCommand(
+								papersManagementService);
 						conferenceUIcommand.execute();
-					}else{
-						throw new InvalidInputException(UIUtils.getText("exception.formato.string"));
+					} else {
+						if (option.contentEquals("S")) {
+							conferenceUIcommand = new PapersSelectionCommand(
+									papersManagementService);
+							conferenceUIcommand.execute();
+						} else {
+							throw new InvalidInputException(
+									UIUtils.getText("exception.formato.string"));
+						}
 					}
-				}
-				
-			}
-		} catch (InvalidInputException e) {
-			System.out.println(e.getMessage());
-		}
-	}
 
+				}
+			} catch (InvalidInputException e) {
+				System.out.println(e.getMessage());
+			}
+
+		} while (!EXIT_CODE.equals(option));
+
+	}
 }
