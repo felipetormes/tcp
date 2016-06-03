d5962af13e19e18ab6ec802f8cb52e6145f3950d
Eduardo <eduardobrito14@gmail.com>
2016-06-03 11:13:15 -0300

    Melhora código do teste de atribuição de nota

--- tcpzin/src/test/ui/text/command/PapersGradeAttributionCommandTest.java
+++ tcpzin/src/test/ui/text/command/PapersGradeAttributionCommandTest.java
@@ -1,35 +1,34 @@
 package test.ui.text.command;
 
-import static org.junit.Assert.*;
-
-
 import org.junit.Before;
 import org.junit.Test;
-
 
 import main.ui.text.command.PapersGradesAttributionCommand;
 import main.business.PapersManagementService;
 import main.business.impl.PapersManagementServiceImpl;
 import main.data.Database;
+import main.exceptions.InvalidNameException;
 
-	public class PapersGradeAttributionCommandTest {
-		
-		PapersGradesAttributionCommand paperGrade;
-		
-		/**
-		 * @throws java.lang.Exception
-		 */
-		@Before
-		public void setUp() throws Exception {
-			Database database = new Database();
-			PapersManagementService paperManagement = new PapersManagementServiceImpl(database);
-			paperGrade = new PapersGradesAttributionCommand(paperManagement);
-		}
-		
-		
-		@Test
-		public void TestGradeAttribuiton() {
-			paperGrade.execute();
-			
-		}
+public class PapersGradeAttributionCommandTest {
+
+	private PapersGradesAttributionCommand paperGrade;
+	private Database database;
+	private PapersManagementService paperManagement;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		database = new Database();
+		paperManagement = new PapersManagementServiceImpl(database);
+		paperGrade = new PapersGradesAttributionCommand(paperManagement);
+
 	}
+
+	@Test
+	public void TestGradeAttribuiton() throws InvalidNameException {
+		paperGrade.execute();
+
+	}
+}
