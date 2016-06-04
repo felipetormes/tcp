ae38c106fe60842ab10264ad39ab85c45848e5eb
Luís Möllmann <luisgustavo181@gmail.com>
2016-06-04 04:21:22 -0300

    cria novo método UIUtils.chooseFromList

--- tcpzin/src/main/ui/text/UIUtils.java
+++ tcpzin/src/main/ui/text/UIUtils.java
@@ -3,6 +3,7 @@ package main.ui.text;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
@@ -13,14 +14,14 @@ public class UIUtils {
 	public static final String PROPERTY_RESOURCE_BUNDLE = "main.resources.globalMessages";
 	private final static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));;
 	private static ResourceBundle bundle;
-	
-	
+
+
 	public UIUtils() {
 		UIUtils.bundle = ResourceBundle.getBundle(PROPERTY_RESOURCE_BUNDLE);
-	
+
 	}
 	public static String getText(String key) {
-		
+
 		String text = null;
 		if (text == null) {
 			try {
@@ -31,7 +32,7 @@ public class UIUtils {
 		}
 		return text;
 	}
-	
+
 	public static Double readDouble(String field) {
 		Double value = null;
 		while (value == null) {
@@ -41,9 +42,9 @@ public class UIUtils {
 			} catch (NumberFormatException nfe) {
 				System.out.println(UIUtils
 						.getText("exception.formato.double"));
-				
+
 			} catch (Exception e) {
-				
+
 			}
 		}
 		return value;
@@ -59,16 +60,16 @@ public class UIUtils {
 			} catch (NumberFormatException nfe) {
 				System.out.println(UIUtils
 						.getText("exception.formato.int"));
-	
+
 			} catch (Exception e) {
-	
+
 			}
 		}
 		return value;
 	}
 
-	
-	public  static String readString(String field) {
+
+	public static String readString(String field) {
 		String value = null;
 		while (value == null) {
 			try {
@@ -76,13 +77,25 @@ public class UIUtils {
 					System.out.print(UIUtils.getText(field) + ": ");
 				value = reader.readLine();
 			} catch (Exception e) {
-	
+
 			}
 		}
 		return value;
 	}
-	
 
-	
+	public static Object chooseFromList(List<? extends Object> list) {
+		int numItems = list.size();
 
+		for (int i = 0; i < numItems; i++) {
+			String option = String.valueOf(i + 1) + ". " + list.get(i);
+			System.out.println(option);
+		}
+
+		int chosen = -1;
+		do {
+			chosen = readInteger("message.choose.option") - 1;
+		} while (chosen < 0 || chosen > numItems);
+
+		return list.get(chosen);
+	}
 }
