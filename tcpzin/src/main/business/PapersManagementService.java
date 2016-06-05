747dca1d052fcc32c354abe4fc61e461bf6d57ae
Luís Möllmann <luisgustavo181@gmail.com>
2016-06-04 21:45:17 -0300

    muda a interface de PapersManagementService

--- tcpzin/src/main/business/PapersManagementService.java
+++ tcpzin/src/main/business/PapersManagementService.java
@@ -6,13 +6,16 @@ import java.util.Map;
 import main.business.domain.Conference;
 import main.business.domain.Paper;
 import main.business.domain.Researcher;
+import main.exceptions.BusinessDomainException;
 
 public interface PapersManagementService {
 
-	public abstract void allocPapersToReviewers(Conference conference, int numReviewers);
+	public abstract Map<Integer, Integer> allocPapersToReviewers(Conference conference, int numReviewers) throws BusinessDomainException;
 
 	public abstract void setGradeToPaper(Paper paper, Researcher reviewer, Double grade);
 
+	public List<Conference> getConferencesWithPendingAllocation();
+	
 	public abstract Map<Paper,Boolean> selectPapersByAverage(Conference conference);
 
 	public abstract List<Conference> getAllConferences();
