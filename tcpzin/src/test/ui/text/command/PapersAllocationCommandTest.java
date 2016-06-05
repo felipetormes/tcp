01bcbf64ad7d3bc4121cc279693bca3dbbe84785
L Gabriel T Neto <L Gabriel T Neto>
2016-06-05 01:53:13 -0300

    Adiciona teste para PapersAllocationCommand

--- tcpzin/src/test/ui/text/command/PapersAllocationCommandTest.java
+++ tcpzin/src/test/ui/text/command/PapersAllocationCommandTest.java
@@ -3,25 +3,28 @@ package test.ui.text.command;
 import main.business.PapersManagementService;
 import main.business.impl.PapersManagementServiceImpl;
 import main.data.Database;
+import main.exceptions.InvalidNameException;
 import main.ui.text.command.PapersAllocationCommand;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class PapersAllocationCommandTest {
 	private Database database;
-	private PapersManagementService pms;
-	private PapersAllocationCommand command;
-	
+	private PapersAllocationCommand paperAllocation;
+	private PapersManagementService paperManagement;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@Before
-	public void setup() {
-		database = new Database();
-		pms = new PapersManagementServiceImpl(database);
-		command = new PapersAllocationCommand(pms);
+	public void setUp() throws Exception {
+		database = new Database(true,"emptyResearches.csv","conferencias.csv","artigos.csv","atribuicoes.csv");
+		paperManagement = new PapersManagementServiceImpl(database);
+		paperAllocation = new PapersAllocationCommand(paperManagement);
 	}
-	
+
 	@Test
-	public void execute() {
-		command.execute();
-	}
+	public void TestGradeAttribuiton() throws InvalidNameException {
+		paperAllocation.execute();
+	}	
 }
