47a961a2dbc43d83059271876927cb981ad8de25
Eduardo Brito <Eduardo Brito>
2016-06-06 21:02:19 -0300

    Implementa a Main

--- tcpzin/src/main/run/ConferenceManagement.java
+++ tcpzin/src/main/run/ConferenceManagement.java
@@ -1,7 +1,17 @@
 package main.run;
 
+import main.business.PapersManagementService;
+import main.business.impl.PapersManagementServiceImpl;
+import main.data.Database;
+import main.ui.ConferenceUI;
+import main.ui.text.ConferenceTextInterface;
+
 public class ConferenceManagement {
 	public static void main(String[] args) {
+		Database database = new Database();
+		PapersManagementService paperManagement = new PapersManagementServiceImpl(database);
+		ConferenceUI conferenceUI = new ConferenceTextInterface(paperManagement);
+		conferenceUI.showUI();
 		
 	}
 }
