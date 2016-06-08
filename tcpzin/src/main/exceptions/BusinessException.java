05a7f88abdfd7b6cd5bee2449f079d03f11a1bad
L Gabriel T Neto <L Gabriel T Neto>
2016-06-07 23:31:26 -0300

    Remove warning do BusinessException

--- tcpzin/src/main/exceptions/BusinessException.java
+++ tcpzin/src/main/exceptions/BusinessException.java
@@ -2,6 +2,11 @@ package main.exceptions;
 
 public abstract class BusinessException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public BusinessException() {
 	}
 	
