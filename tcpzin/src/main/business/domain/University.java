5649c9586f12fcb300baf3f569b84dd502f7a8d9
Luís Möllmann <luisgustavo181@gmail.com>
2016-06-07 23:49:27 -0300

    troca exceção

--- tcpzin/src/main/business/domain/University.java
+++ tcpzin/src/main/business/domain/University.java
@@ -1,15 +1,16 @@
 package main.business.domain;
 
+import main.exceptions.BusinessDomainException;
 import main.exceptions.InvalidNameException;
 
 public class University {
 	private String name;
 	
-	public University(String name) throws InvalidNameException {
+	public University(String name) throws BusinessDomainException {
 		if (name != null) {
 			this.name = name;
 		} else {
-			throw new InvalidNameException("Invalid university name.");
+			throw new BusinessDomainException("exception.business.domain.invalidUniversity");
 		}
 	}
 	
