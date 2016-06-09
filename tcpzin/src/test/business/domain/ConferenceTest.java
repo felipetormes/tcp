9d622ff4f9ee9f9558183af7ebb4c6661900b64c
Luís Möllmann <luisgustavo181@gmail.com>
2016-06-09 16:52:13 -0300

    muda ConferenceTest

--- tcpzin/src/test/business/domain/ConferenceTest.java
+++ tcpzin/src/test/business/domain/ConferenceTest.java
@@ -1,8 +1,10 @@
 package test.business.domain;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import main.business.domain.Conference;
@@ -16,12 +18,12 @@ import org.junit.Test;
 
 public class ConferenceTest {
 	Database database;
-	Map<Researcher, Integer> allocSoFar;
+	Map<Researcher, List<Paper>> reviewer2papers;
 	
 	@Before
 	public void setup() throws BusinessDomainException {
 		database = buildDatabase("issue26/case1/");
-		allocSoFar = buildAllocSoFar();
+		reviewer2papers = buildReviewer2papers();
 	}
 	
 	@Test
@@ -34,20 +36,25 @@ public class ConferenceTest {
 		Paper paper = database.getPaperById(Aid);
 		
 		Conference c = database.getConferenceByInitials("C1");
-		int bestCandidateId = c.chooseBestCandidate(paper, allocSoFar).getId();
+		int bestCandidateId = c.chooseBestCandidate(paper, reviewer2papers).getId();
 		
 		return bestCandidateId;
 	}
 	
-	private Map<Researcher, Integer> buildAllocSoFar() {
-		Map<Researcher, Integer> allocSoFar = new HashMap<Researcher, Integer>();
-		allocSoFar.put(database.getResearcherById(1), 1);
-		allocSoFar.put(database.getResearcherById(2), 1);
-		allocSoFar.put(database.getResearcherById(3), 2);
-		allocSoFar.put(database.getResearcherById(4), 1);
-		allocSoFar.put(database.getResearcherById(6), 2);
+	private Map<Researcher, List<Paper>> buildReviewer2papers() {
+		Map<Researcher, List<Paper>> reviewer2papers = new HashMap<Researcher, List<Paper>>();
+		for (int id = 1; id <= 6; id++) {
+			reviewer2papers.put(database.getResearcherById(id), new ArrayList<Paper>());
+		}
+		reviewer2papers.get(database.getResearcherById(2)).add(database.getPaperById(1));
+		reviewer2papers.get(database.getResearcherById(3)).add(database.getPaperById(1));
+		reviewer2papers.get(database.getResearcherById(4)).add(database.getPaperById(2));
+		reviewer2papers.get(database.getResearcherById(3)).add(database.getPaperById(2));
+		reviewer2papers.get(database.getResearcherById(1)).add(database.getPaperById(3));
+		reviewer2papers.get(database.getResearcherById(6)).add(database.getPaperById(4));
+		reviewer2papers.get(database.getResearcherById(6)).add(database.getPaperById(5));
 		
-		return allocSoFar;
+		return reviewer2papers;
 	}
 	
 	private Database buildDatabase(String root) throws BusinessDomainException {
